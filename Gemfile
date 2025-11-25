df7497eea561a2de76379f3b8fb0c4db3b65282f
Gary-mac <glswim@gmail.com>
2025-11-25 22:57:52 +0800

    Display actual messages

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'semantic-ui-sass', '~> 2.4.1'
 
 gem 'jquery-rails'
+
+gem 'hirb', '~> 0.7.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(username: "Jasmine", password: "password")
+# User.create(username: "Jenny", password: "password")
+# User.create(username: "Alice", password: "password")
+# User.create(username: "Thomas", password: "password")
+# User.create(username: "Eric", password: "password")
+
+Message.create(body: "This is a serious test", user: User.last)
+Message.create(body: "This is another test", user: User.first)
+Message.create(body: "This is an easy test", user: User.last)
+Message.create(body: "This is a cool test", user: User.first)
+Message.create(body: "This is a super sonic test", user: User.last)
+Message.create(body: "This is a tumultuous test", user: User.last)
